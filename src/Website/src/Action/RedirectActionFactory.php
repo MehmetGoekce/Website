580655e876eb19e7a2796b0e9580994124405c6d
Mehmet Gökce <mg@mehmetgoekce.com>
2017-04-24 10:41:14 +0200

    invoke librarys, change default

--- src/Website/src/Action/RedirectActionFactory.php
+++ src/Website/src/Action/RedirectActionFactory.php
@@ -5,6 +5,7 @@ namespace Website\Action;
 use Interop\Container\ContainerInterface;
 use Zend\ServiceManager\Factory\FactoryInterface;
 use Website\Action\RedirectAction;
+use Zend\Expressive\Router\RouterInterface;
 
 class RedirectActionFactory implements FactoryInterface
 {
@@ -16,6 +17,6 @@ class RedirectActionFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        return new RedirectAction($container->get(\Zend\Expressive\Router\RouterInterface::class));
+        return new RedirectAction($container->get(RouterInterface::class));
     }
 }
